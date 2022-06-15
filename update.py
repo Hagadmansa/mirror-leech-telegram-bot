65be6c1624f801ee227801b6d05669481774b15f
nsuwhsuwshu <107324303+nsuwhsuwshu@users.noreply.github.com>
2022-06-15 16:17:37 +0530

    Update update.py

--- update.py
+++ update.py
@@ -36,7 +36,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = None
+    UPSTREAM_REPO = "https://github.com/nsuwhsuwshu/mirror-leech-telegram-bot"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
